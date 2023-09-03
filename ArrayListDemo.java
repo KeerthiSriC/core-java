66b8963ed12316faee6ad1ade170f0ba2c76e0a6
KeerthiSriC <keerthi.rgukt@gmail.com>
2023-09-03 19:01:14 +0530

    Array list demo practice

---
+++ ArrayListDemo.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayListDemo 
+{
+    public static void main(String[] args)
+    {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(2);
+        myList.add(4);
+        myList.add(6);
+        myList.add(10);
+        
+       System.out.println(myList);
+    }
+    
+}
