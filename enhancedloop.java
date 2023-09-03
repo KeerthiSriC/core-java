bef8a089008d4dcb52e3a995eea703181958819c
KeerthiSriC <keerthi.rgukt@gmail.com>
2023-09-03 18:03:36 +0530

    enhanced for loop

---
+++ enhancedloop.java
@@ -0,0 +1,10 @@
+class EnhancedForLoop
+{
+    public static void main(String[] args)
+    {
+        int[] myNum = {10, 20, 30, 40};
+         for (Integer n : myNum){
+            System.out.println(n);
+         }
+    }
+}
